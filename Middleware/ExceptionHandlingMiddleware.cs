3889fb8046d359ce972bbe793cd4f2b9008abebb
Chaitanya Chetu <Chaitanya.chetu@entertainment.com.au>
2024-10-22 16:23:59 -0400

    Add global exception handling middleware

---
+++ Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ContactsApi.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "An unexpected error occurred.",
+                Detailed = exception.Message
+            };
+
+            var jsonResponse = JsonConvert.SerializeObject(response);
+            return context.Response.WriteAsync(jsonResponse);
+        }
+    }
+}
